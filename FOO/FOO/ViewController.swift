769b64d8cd44f35e89bf6c365339da30419d5476
“BestRV” <354223560@qq.com>
2016-12-30 15:53:39 +0800

    今天辛启武

---
+++ FOO/FOO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FOO
+//
+//  Created by Apple-Mini on 16/12/30.
+//  Copyright © 2016年 qinghua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
